71c643b43c22554c5b8755d80b554134ed2bf413
N0ViP <01.xx.vip@gmail.com>
2025-01-17 23:39:54 +0100

    added memcpy

---
+++ ft_memset.c
@@ -0,0 +1,31 @@
+#include <stdio.h>	//for size_t
+
+void	*ft_memset(void *str, int c, size_t n)
+{
+	register void			*s;
+	register size_t			r;
+	register size_t			l;
+	register unsigned char	x;
+
+	s = str;
+	l = n;
+	x = (unsigned char) c;
+	r = 0x0101010101010101 * x;
+	while (l && (l % 8 != 0))
+	{
+		*((char *) s++) = x;
+		l--;
+	}
+	while (l >= sizeof(size_t))
+	{
+		*((size_t *) s) = r;
+		l -= sizeof(size_t);
+		s += sizeof(size_t);
+	}
+	while (l)
+	{
+		*((char *) s++) = x;
+		l--;
+	}
+	return (str);
+}
